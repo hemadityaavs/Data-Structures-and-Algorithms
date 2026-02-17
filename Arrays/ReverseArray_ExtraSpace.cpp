5f759bed46d4a1097cf5d2746332bf49608ab6d9
adityaathotagit config --global user.email adityaathota000@gmail.com <adityaathota000@gmail.com>
2026-02-17 14:49:11 +0530

    Reverse Array implementations (extra space and in-place)

---
+++ Arrays/ReverseArray_ExtraSpace.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int* Reverse(int * arr,int n){
+    int reversed[5];
+
+    for(int i=n-1;i>=0;i--){
+        reversed[n-1-i]=arr[i];
+    }
+    for(int i=0;i<n;i++){
+        arr[i]=reversed[i];
+    }
+
+    return arr;
+
+}
+
+int main(){
+    int a[]={5,4,3,9,2};
+    int n=sizeof(a)/sizeof(int);
+    int* a_reversed=Reverse(a,n);
+
+    for(int i=0;i<n;i++){
+        cout<<a_reversed[i];
+    }
+    return 0;
+}
---
+++ Arrays/ReverseArray_NoExtra.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int* Reverse(int *arr,int n){
+    int start=0, end=n-1;
+
+    while(start<end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+
+    return arr;
+}
+int main(){
+    int a[]={5,4,3,9,2};
+    int n=sizeof(a)/sizeof(int);
+    Reverse(a,n);
+    for(int i=0;i<n;i++){
+        cout<<a[i];
+    }
+
+    return 0;
+}
