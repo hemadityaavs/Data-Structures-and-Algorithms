eafb798e16b532ae88d9d5f7ac23be3210250358
adityaathotagit config --global user.email adityaathota000@gmail.com <adityaathota000@gmail.com>
2026-02-17 14:55:22 +0530

    Print Subarrays implementation

---
+++ Arrays/PrintSubArrays.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+void PrintSubArray(int *arr,int n){
+    int i,j,k;
+    for(i=0;i<n;i++){
+        for(j=i;j<n;j++){
+            for(k=i;k<=j;k++){
+                cout<<arr[k];
+            }
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void PrintSubArray1(int *arr,int n){
+    int i,j,k;
+    for(i=0;i<n;i++){
+        for(j=0;j<=i;j++){
+            for(k=j;k<=i;k++){
+                cout<<arr[k];
+            }
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int a[]={5,4,3,9,2};
+    int n=sizeof(a)/sizeof(int);
+    PrintSubArray(a,n);
+    cout<<endl;
+    PrintSubArray1(a,n);
+    return 0;
+}
